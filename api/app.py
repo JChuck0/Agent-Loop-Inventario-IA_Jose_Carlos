c2f57e583bb39092555c9a22bab60384250d6609
JChuck0 <110050395+JChuck0@users.noreply.github.com>
2026-09-24 11:10:30 +0000

    Creamos el endpoint Post

--- api/app.py
+++ api/app.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 import csv
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    name :str
+    quantity : int
+    unit : str
 
 app = FastAPI()
 
@@ -9,3 +15,11 @@ def get_inventory():
     with open("products.csv", "r", encoding = "utf-8") as file:
         reader = csv.DictReader(file)
         return list(reader)
+
+@app.post("/inventory")
+def post_inventory(product : Product):
+    with open("poducts.csv", "a", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, filenames = ["id", "name", "quantity", "unit"])
+    pass
+
+
